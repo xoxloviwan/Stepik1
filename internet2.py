929bc8909b1bd415dbe8fe1962fdf0526aecb38d
xoxloviwan <43066471+xoxloviwan@users.noreply.github.com>
2018-09-10 21:58:21 +0300

    it works!!!

--- internet2.py
+++ internet2.py
@@ -4,21 +4,21 @@ import requests
 # import sys
 #
 # sys.stdin = open("C:/Users/xoxlo/Desktop/tester.txt",'r')
-link_pattern = re.compile(r'<a[^>]*?href=[\'\"]?'
-                          r'(?:.*://)?'
-                          r'(\w[\w.\-]*)')
+link_pattern = re.compile(r'(?:<a[^>]*?href=[\'\"]?)'
+                          r'(?:\w*?://)'
+                          r'(\w[\w\.\-]*)')
 res = requests.get(input())
-links = set()
+domains = set()
 for line in res.text.splitlines():
     line = line.rstrip()
-    link = re.findall(link_pattern, line)
-    if link:
-        links.add(*link)
+    links = re.findall(link_pattern, line)
+    if links:
+        for link in links:
+            domains.add(link)
 lst = list()
-for link in links:
+for link in domains:
     lst.append(link)
 lst.sort()
 for el in lst:
     print(el)
-
 # http://pastebin.com/raw/7543p0ns
