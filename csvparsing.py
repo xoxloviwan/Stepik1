72e811cdf5ba5c4993b663103aacd3b7a99fea78
xoxloviwan <43066471+xoxloviwan@users.noreply.github.com>
2018-09-11 00:32:42 +0300

    test commit

--- csvparsing.py
+++ csvparsing.py
@@ -6,9 +6,6 @@ link = 'https://stepik.org/media/attachments/lesson/24473/Crimes.csv'
 res = requests.get(link)
 reader = csv.DictReader(res.text.splitlines())
 cnt = collections.Counter()
-# for i in range(500):
-#     dct = next(reader)
-#     print(dct)
 for dct in reader:
     if '2015' in dct.get('Date'):
         cnt[dct.get('Primary Type')] += 1
